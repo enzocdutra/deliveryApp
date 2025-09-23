f9e2dea422d9889a5dc62d61c3f7c2988f19c92b
enzo <enzocampaodutra2310@gmail.com>
2025-09-23 04:18:07 -0300

    teste

--- backend/server.js
+++ backend/server.js
@@ -5,12 +5,14 @@ import path from "path";
 import productRoutes from "./routes/productRoutes.js";
 import authRoutes from "./routes/authRoutes.js";
 import dotenv from "dotenv";
+
+dotenv.config();
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const PORT = process.env.PORT || 5000;
-dotenv.config();
+
 // Middlewares globais
 app.use(express.json({ limit: "15mb" }));
 app.use(cors());
@@ -19,10 +21,9 @@ app.use(cors());
 app.get("/", (req, res) => {
   res.json({ ok: true, api: "Hamburgueria API", version: "1.0.0" });
 });
-app.use("/produtos", productRoutes);
 
+app.use("/produtos", productRoutes);
 app.use("/auth", authRoutes);
-// Start
-app.listen(PORT, () => {
-  console.log(`🚀 API rodando em http://localhost:${PORT}`);
-});
+
+// ❌ NÃO usa app.listen no Vercel
+export default app;
